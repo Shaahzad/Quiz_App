c7f13783fabf98bca975bc2132911437997f5653
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-10-31 22:55:42 +0500

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,88 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef, useState } from 'react'
+import './index.css'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+ const [questions, setQuestions] = useState(null)
+ const [currentIndex, setCurrentIndex] = useState(0)
+ const [score, setScore] = useState(0)
+ const Input = useRef([])
+ const navigate = useNavigate()
 
+  useEffect(()=>{
+     axios('https://the-trivia-api.com/v2/questions')
+      .then((res)=>{
+        setQuestions(res.data)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  },[])
+
+
+
+
+  const NextQuestion = () => {
+    const selectedOption = Input.current.find(item => item && item.checked)
+    if(selectedOption === questions[currentIndex].correctAnswer){
+      setScore()
+    }else{
+      setScore()
+    }
+
+    if(currentIndex < questions.length - 1){
+      setCurrentIndex(currentIndex + 1) 
+      return
+    }
+    navigate('QuizScore')
+  }
+
+
+
+
+
+  function shuffleArray(arr){
+    const emptyArr = []
+    const shuffleArr = []
+    for (let index = 0; index < arr.length; index++) {
+        const randomNumber = Math.floor(Math.random() * arr.length)
+        if(emptyArr.includes(randomNumber)){
+          index--
+        }else{
+          emptyArr.push(randomNumber)
+          shuffleArr[randomNumber] = arr[index]
+        }
+      } 
+      return shuffleArr
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-3xl font-bold text-center mt-10'>Quiz App {`${100} / ${score}`}</h1>
+      {
+        questions ? <div>
+          <h1 className='text-2xl m-10 bg-red-300 p-5 font-bold'>Q{currentIndex + 1} : 
+          {questions[currentIndex].question.text}</h1>
+          {shuffleArray([...questions[currentIndex].incorrectAnswers, 
+           questions[currentIndex].correctAnswer]).map((item,index)=>{
+            return(
+              <div key={index} className='flex gap-5 ml-10'>
+                <input type="radio" value={item} id={index} name='question' className='w-4' 
+                 ref={el => Input.current[index] = el}/>
+                <label htmlFor={index} className='text-2xl'>{item}</label>
+              </div>
+            )
+          })}
+          <button onClick={NextQuestion} className='text-2xl m-10 bg-blue-300 p-5 font-bold rounded-2xl'>Next</button>
+        </div> : <h1 className='text-3xl font-bold text-center mt-10'>Loading...</h1>
+      }
     </>
   )
 }
+      
+
 
 export default App
+
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Score from './Score'
+
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<>
+<BrowserRouter>
+<Routes>
+    <Route path='/' element={<App/>}/>
+    <Route path='QuizScore' element={<Score/>}/>
+</Routes>
+</BrowserRouter>
+</>
 )
